98effe520ec43f4a15f24163153c7682b38f2443
cheerlee <qq302969377@163.com>
2022-05-07 05:44:08 +0000

    删除文件 test

--- test/src/1.js
+++
@@ -1,9 +0,0 @@
-const element2 = React.createElement(
-  'h1',
-  {className: 'active'},
-  'Hello world!' // babel会将JSX转换为对react.createElement()的调用
-)
-ReactDOM.render(
-  element2,
-  document.getElementById('root2')
-)
